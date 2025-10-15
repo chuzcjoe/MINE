7a5bf1aa26251122663a59a1e48844806d75dc21
joechu <44826941+chuzcjoe@users.noreply.github.com>
2025-10-14 22:31:25 -0700

    dump weights to bin files

--- python/networks/FCN.py
+++ python/networks/FCN.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+import argparse
 
 
 class FCN(nn.Module):
@@ -49,7 +51,37 @@ def load_and_inference(path: str):
     for i in range(batch_size):
         print("batch: {}, output: {}".format(i, outputs[i].detach().cpu()))
 
+def load_and_dump_weights(path: str):
+    print("loading FCN weights from {}".format(path))
+    model = FCN()
+    model.load_state_dict(torch.load(path, map_location="cpu"))
+    weights = []
+    for name, param in model.named_parameters():
+        print(f"{name}: {param.shape}, {param.data[:10]}")
+        weights.append(param.detach().cpu().numpy().ravel())
+    flat = np.concatenate(weights, axis=0)
+    flat.astype(np.float32).tofile("./fcn_weights.bin")
+    print("saved fcn weights to fcn_weights.bin")
+
+def load_weights_bin(path: str):
+    weights = np.fromfile(path, dtype=np.float32)
+    print(weights[:10])
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Utility CLI for FCN.")
+    parser.add_argument("--test", action="store_true", help="Run the FCN unit test.")
+    parser.add_argument("--load", action="store_true", help="load pt and inference")
+    parser.add_argument("--dump", action="store_true", help="dump weights to a bin file")
+    parser.add_argument("--show_weights", action="store_true", help="load weights bin file")
+    args = parser.parse_args()
+
     torch.manual_seed(1)
-    # test_fcn_with_random_weights(True)
-    load_and_inference("../ckpts/fcn_weights.pt")
+
+    if args.test:
+        test_fcn_with_random_weights(True)
+    if args.load:
+        load_and_inference("../ckpts/fcn_weights.pt")
+    if args.dump:
+        load_and_dump_weights("../ckpts/fcn_weights.pt")
+    if args.show_weights:
+        load_weights_bin("./fcn_weights.bin")
